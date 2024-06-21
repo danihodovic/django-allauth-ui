5a8597e26d0d0b528f13f14a210de955ebced447
Dani Hodovic <danihodovic@users.noreply.github.com>
2024-06-21 19:51:36 -0300

    docs: fix linting of sample_project (#73)

--- sample_deployment/sample_deployment/example/views.py
+++ sample_deployment/sample_deployment/example/views.py
@@ -10,9 +10,9 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
-            social_account = SocialAccount.objects.filter(
+            social_account = SocialAccount.objects.filter(  # pylint: disable=no-member
                 user=self.request.user
-            ).first()  # pylint: disable=no-member
+            ).first()
             if social_account:
                 context["extra_data"] = social_account.extra_data
         return context
@@ -24,9 +24,9 @@ class SocialAccountMetadataView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
-            social_account = SocialAccount.objects.filter(
+            social_account = SocialAccount.objects.filter(  # pylint: disable=no-member
                 user=self.request.user
-            ).first()  # pylint: disable=no-member
+            ).first()
             if social_account:
                 context["extra_data"] = social_account.extra_data
                 json_str = json.dumps(social_account.extra_data, indent=2)
