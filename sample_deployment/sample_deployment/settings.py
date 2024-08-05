5391797e85b1cf5b291e374c192c183ff731454b
Dani Hodovic <danihodovic@users.noreply.github.com>
2024-08-05 15:09:21 -0300

    docs: enable logging for sample_deployment (#103)

--- sample_deployment/sample_deployment/settings.py
+++ sample_deployment/sample_deployment/settings.py
@@ -186,3 +186,30 @@ STORAGES = {
 }
 
 ALLAUTH_UI_THEME = "business"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"},
+    },
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(levelname)s %(asctime)s %(module)s "
+                "%(process)d %(thread)d %(message)s"
+            )
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
